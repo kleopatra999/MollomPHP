db56f77444693fe67d775baaca6f9e46e6b21e72
nickveenhof <nick.veenhof@gmail.com>
2014-09-10 11:24:44 -0400

    Adding the exception classes

--- src/Client/Client.php
+++ src/Client/Client.php
@@ -7,13 +7,14 @@
  * @license MIT|GNU GPL v2
  *   See LICENSE-MIT.txt or LICENSE-GPL.txt shipped with this library.
  */
- 
- namespace Mollom\Client;
+namespace Mollom\Client;
+
+use \Mollom\Exception\MollomException;
 
 /**
  * The base class for Mollom client implementations.
  */
-abstract class Mollom {
+abstract class Client {
   /**
    * The Mollom API version, used in HTTP requests.
    */
@@ -966,7 +967,9 @@ abstract class Mollom {
     }
     if (isset($input)) {
       preg_match_all('@([^, =]+)="([^"]*)"@', $input, $header);
-      $header = array_combine($header[1], $header[2]);
+      if (!empty($header[1]) && !empty($header[2])) {
+        $header = array_combine($header[1], $header[2]);
+      }
     }
     return $header;
   }
@@ -1148,10 +1151,8 @@ abstract class Mollom {
    *   - contextTitle: The title of the parent/context content of the stored
    *     content; e.g., the title of the article or forum thread a comment is
    *     posted on (not the parent comment that was replied to).
-   *   - contextCreated: The creation date of the parent/context content of the
-   *     stored content as a unix timestamp in seconds; e.g., the creation date
-   *     of the article or forum thread a comment is posted on (not the parent
-   *     comment that was replied to).
+   *   - trackingImageId: An optional string identifier used to request an
+   *     image beacon.  This is used for form behavior analysis.
    *
    * @return mixed
    *   On success, an associative array representing the full content record,
@@ -1286,6 +1287,10 @@ abstract class Mollom {
    *     - profanity: The content contains obscene, violent, profane language.
    *     - quality: The content is of low quality.
    *     - unwanted: The content is unwanted, taunting, off-topic.
+   *   - type (optional): A string denoting the type of feedback submitted.
+   *     - moderate: Feedback from the admin moderation process (default).
+   *     - flag: feedback from end users flagging content as inappropriate
+   *   - source (optional): A string denoting the user-interface source of the feedback.
    *   and at least one of:
    *   - contentId: A Mollom content ID associated with the content.
    *   - captchaId: A Mollom CAPTCHA ID associated with the content.
@@ -1413,151 +1418,27 @@ abstract class Mollom {
     $result = $this->query('POST', $path);
     return $this->lastResponseCode === TRUE;
   }
-}
-
-/**
- * A catchable Mollom exception.
- *
- * The Mollom class internally uses exceptions to handle HTTP request errors
- * within the Mollom::handleRequest() method. All exceptions thrown in the
- * Mollom class and derived classes should be instances of the MollomException
- * class if they pertain to errors that can be catched/handled within the class.
- * Other errors should not use the MollomException class and handled
- * differently.
- *
- * No MollomException is supposed to pile up as a user-facing fatal error. All
- * functions that invoke Mollom::handleRequest() have to catch Mollom
- * exceptions.
- *
- * @see Mollom::query()
- * @see Mollom::handleRequest()
- *
- * @param $message
- *   The Exception message to throw.
- * @param $code
- *   The Exception code.
- * @param $previous
- *   (optional) The previous Exception, if any.
- * @param $instance
- *   The Mollom class instance the Exception is thrown in.
- * @param $arguments
- *   (optional) A associative array containing information about a performed
- *   HTTP request that failed:
- *   - request: (string) The HTTP method and URI of the performed request; e.g.,
- *     "GET http://server.mollom.com/v1/foo/bar". In case of GET requests, do
- *     not add query parameters to the URI; pass them in 'data' instead.
- *   - data: (array) An associative array containing HTTP GET/POST/PUT request
- *     query parameters that were sent to the server.
- *   - response: (mixed) The server response, either as string, or the already
- *     parsed response; i.e., an array.
- */
-class MollomException extends Exception {
-  /**
-   * @var Mollom
-   */
-  protected $mollom;
 
   /**
-   * The severity of this exception.
+   * Generates a URL to a tracking image.
    *
-   * By default, all exceptions should be logged and appear as errors (unless
-   * overridden by a later log entry).
+   * This image can be used by form behavior analysis to analyze the
+   * humanity of the content author.
    *
-   * @var string
+   * @return array
+   *   An associative array of tracking information generated.
+   *   - tracking_url: the url to the tracking image (without any http protocol)
+   *   - tracking_id: the tracking id generated
    */
-  protected $severity = 'error';
+  public function getTrackingImage() {
+    $public_key = $this->publicKey;
+    $ts = time();
+    $tracking_id = hash_hmac('sha1', $public_key . $ts, $this->privateKey);
 
-  /**
-   * Overrides Exception::__construct().
-   */
-  function __construct($message = '', $code = 0, Exception $previous = NULL, Mollom $mollom, array $request_info = array()) {
-    // Fatal error on PHP <5.3 when passing more arguments to Exception.
-    if (version_compare(phpversion(), '5.3') >= 0) {
-      parent::__construct($message, $code, $previous);
-    }
-    else {
-      parent::__construct($message, $code);
-    }
-    $this->mollom = $mollom;
-
-    // Set the error code on the Mollom class.
-    $mollom->lastResponseCode = $code;
-
-    // Log the exception.
-    // To aid Mollom technical support, include the IP address of the server we
-    // tried to reach in case a request fails.
-    // PHP's native gethostbyname() is available on all platforms, but its DNS
-    // lookup and caching behavior is undocumented and unclear. User comments on
-    // php.net mention that it does not have an own cache and also does not use
-    // the OS/platform's native DNS name resolver. Due to that, we only use it
-    // under error conditions.
-    $message = array(
-      'severity' => $this->severity,
-      'message' => 'Error @code: %message (@server-ip)',
-      'arguments' => array(
-        '@code' => $code,
-        '%message' => $message,
-        '@server-ip' => gethostbyname($mollom->server),
-      ),
+    $result = array(
+      'tracking_url' => '//' . $this->server . '/v1/tracking/' . urlencode($tracking_id) . '.png?public_key=' . $public_key . "&timestamp=" . $ts,
+      'tracking_id' => $tracking_id,
     );
-    // Add HTTP request information, if available.
-    if (!empty($request_info)) {
-      $message += $request_info;
-    }
-    $mollom->log[] = $message;
+    return $result;
   }
-}
-
-/**
- * Mollom network error exception.
- *
- * Thrown in case a HTTP request results in code <= 0, denoting a low-level
- * communication error.
- */
-class MollomNetworkException extends MollomException {
-  /**
-   * Overrides MollomException::$severity.
-   *
-   * The client may be able to recover from this error, so use a warning level.
-   */
-  protected $severity = 'warning';
-}
-
-/**
- * Mollom authentication error exception.
- *
- * Thrown in case API keys or other authentication parameters are invalid.
- */
-class MollomAuthenticationException extends MollomException {
-}
-
-/**
- * Mollom error due to bad client request exception.
- *
- * Thrown in case the local time diverges too much from UTC.
- *
- * @see Mollom::TIME_OFFSET_MAX
- * @see Mollom::REQUEST_ERROR
- * @see Mollom::handleRequest()
- */
-class MollomBadRequestException extends MollomException {
-}
-
-/**
- * Mollom server response exception.
- *
- * Thrown when a request to a Mollom server succeeds, but the response does not
- * contain an expected element; e.g., a backend configuration or execution
- * error that possibly exists on one server only.
- *
- * @see Mollom::handleRequest()
- */
-class MollomResponseException extends MollomException {
-  /**
-   * Overrides MollomException::$severity.
-   *
-   * Might be a client-side error, but more likely a server-side error. The
-   * client may be able to recover from this error.
-   */
-  protected $severity = 'debug';
 }
---
+++ src/Exception/MollomAuthenticationException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\mollom\Exception;
+
+/**
+ * Mollom authentication error exception.
+ *
+ * Thrown in case API keys or other authentication parameters are invalid.
+ */
+class MollomAuthenticationException extends MollomException {
+}
